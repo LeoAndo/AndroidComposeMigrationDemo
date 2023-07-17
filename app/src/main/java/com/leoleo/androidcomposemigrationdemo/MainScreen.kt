1e8166f250791e6b23142f6cdedd68f8065edfae
ando <leo81@outlook.jp>
2023-07-18 04:17:38 +0900

    不要なViewのファイルを削除

--- app/src/main/java/com/leoleo/androidcomposemigrationdemo/MainScreen.kt
+++ app/src/main/java/com/leoleo/androidcomposemigrationdemo/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.leoleo.androidcomposemigrationdemo
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -7,17 +8,13 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import com.leoleo.androidcomposemigrationdemo.ui.AppSurface
 import com.leoleo.androidcomposemigrationdemo.ui.PreviewPhoneDevice
 import com.leoleo.androidcomposemigrationdemo.ui.dashboard.DashboardScreen
@@ -27,10 +24,10 @@ import com.leoleo.androidcomposemigrationdemo.ui.home.HomeViewModel
 import com.leoleo.androidcomposemigrationdemo.ui.notifications.NotificationsScreen
 import com.leoleo.androidcomposemigrationdemo.ui.notifications.NotificationsViewModel
 
-enum class BottomItem(val label: String, val icon: ImageVector) {
-    Home("Home", Icons.Filled.Home),
-    Dashboard("Dashboard", Icons.Filled.AccountBox),
-    Notifications("Notifications", Icons.Filled.Notifications)
+enum class BottomItem(val label: String, @DrawableRes val iconId: Int) {
+    Home("Home", R.drawable.ic_home_black_24dp),
+    Dashboard("Dashboard", R.drawable.ic_dashboard_black_24dp),
+    Notifications("Notifications", R.drawable.ic_notifications_black_24dp)
 }
 
 @Composable
@@ -53,7 +50,12 @@ fun MainScreen(
                 BottomNavigation {
                     items.forEachIndexed { index, item ->
                         BottomNavigationItem(
-                            icon = { Icon(item.icon, contentDescription = item.label) },
+                            icon = {
+                                Icon(
+                                    painterResource(id = item.iconId),
+                                    contentDescription = item.label
+                                )
+                            },
                             label = { Text(item.label) },
                             selected = selectedIndex == index,
                             onClick = { selectedIndex = index }
