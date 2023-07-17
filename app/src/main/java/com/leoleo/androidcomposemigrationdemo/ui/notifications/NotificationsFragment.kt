909e1181a1cc86ba9cec161c9916cca4e073eaf3
ando <leo81@outlook.jp>
2023-07-17 23:44:57 +0900

    viewとcomposeのtheme比較ができるようにstyleをカスタマイズする

--- app/src/main/java/com/leoleo/androidcomposemigrationdemo/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/leoleo/androidcomposemigrationdemo/ui/notifications/NotificationsFragment.kt
@@ -1,15 +1,14 @@
 package com.leoleo.androidcomposemigrationdemo.ui.notifications
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import com.leoleo.androidcomposemigrationdemo.R
 import com.leoleo.androidcomposemigrationdemo.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -17,22 +16,16 @@ class NotificationsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+    private val viewModel by viewModels<NotificationsViewModel>()
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentNotificationsBinding.bind(view)
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
-        return root
     }
 
     override fun onDestroyView() {
